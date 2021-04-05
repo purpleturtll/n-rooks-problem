41856c6d2ba85d5813603fe949a602e1cc08fd24
Bartosz Ciesielczyk <purpleturtll@gmail.com>
2021-04-05 19:22:58 +0200

    dodanie animacji wieży

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-let board_state = []
-let tile_size = 32
+let tile_size = 64
 let board_size = 8
 let spacing = 0
+let rook_img;
+let rooks = []
+let speed = 0.1
 
-let state = {
-    "empty": 0,
-    "rook": 1
-}
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
-function setup_board(){
-    for(let i = 0; i < board_size; i++){
-        board_state.push([])
-        for(let j = 0; j < board_size; j++){
-            board_state[i].push(state.empty)
+function Rook(x, y){
+    this.index_pos = {x: x, y: y}
+    this.pos = {x: Math.round((spacing * (this.index_pos.x + 1)) + (tile_size * this.index_pos.x)), y: Math.round((spacing * (this.index_pos.y + 1)) + (tile_size * this.index_pos.y))}
+    this.index_destination = {x: this.index_pos.x, y: this.index_pos.y}
+    this.destination = {x: this.pos.x, y: this.pos.y}
+
+    Rook.prototype.set_destination = (x, y) => {
+        this.index_destination = {x: x, y: y}
+        this.destination = {x: Math.round((spacing * (this.index_destination.x + 1)) + (tile_size * this.index_destination.x)), y: Math.round((spacing * (this.index_destination.y + 1)) + (tile_size * this.index_destination.y))}
+    }
+
+    Rook.prototype.animate = () => {
+        if(Math.round(this.pos.x) != Math.round(this.destination.x)){
+            this.pos.x += (this.destination.x - this.pos.x) * speed
+        }else{
+            if(this.pos.y != this.destination.y){
+                this.pos.y += (this.destination.y - this.pos.y) * speed
+            }
         }
     }
+
+    Rook.prototype.atDestination = () => {
+        return Math.round(this.pos.x) == Math.round(this.destination.x) && Math.round(this.pos.y) == Math.round(this.destination.y)
+    }
+}
+
+function preload() {
+    rook_img = loadImage('rook.png');
 }
 
 function draw_board(){
@@ -33,17 +57,39 @@ function draw_board(){
                 fill(color("#769656"))
             }
             white = !white
-            square((spacing * (i + 1)) + (tile_size * i), (spacing * (j + 1)) + (tile_size * j), tile_size)
+            square((spacing * (i + 1)) + (tile_size * i), (spacing * (j + 1)) + (tile_size * j) , tile_size)
+        }
+    }
+}
+
+function draw_pieces(){
+    for(let i = 0; i < rooks.length; i++){
+        image(rook_img, rooks[i].pos.x, rooks[i].pos.y, tile_size, tile_size)
+    }
+}
+
+function animatePieces(){
+    for(let i = 0; i < rooks.length; i++){
+        if(!rooks[i].atDestination()){
+            rooks[i].animate()
         }
     }
 }
 
 function setup() {
-    createCanvas((tile_size * board_size) + (spacing * (board_size + 1)), (tile_size * board_size) + (spacing * (board_size + 1)));
-    setup_board()
+    createCanvas((tile_size * board_size) + (spacing * (board_size + 1)), (tile_size * board_size) + (spacing * (board_size + 1)), "WEBGL");
+    //               [col][row]
+    rooks.push(new Rook(1, 2))
 }
 
 function draw() {
     background(220);
+    animatePieces()
     draw_board()
+    draw_pieces()
+    for(let i = 0; i < rooks.length; i++){
+        if(rooks[i].atDestination()){
+            rooks[i].set_destination(getRandomInt(0, board_size), getRandomInt(0, board_size))
+        }
+    }
 }
